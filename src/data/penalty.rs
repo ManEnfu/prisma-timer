41cbe0a69fd57e355210aeb6e2ace9e8564f3b6f
ManEnfu <manenfu@protonmail.com>
2024-01-14 21:46:00 +0700

    feat: derive `glib::Enum` for `Penalty` to be usable as object properties

--- src/data/penalty.rs
+++ src/data/penalty.rs
@@ -1,12 +1,18 @@
+use gtk::glib;
+
 /// Penalty of a solve.
-#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, glib::Enum)]
+#[enum_type(name = "PtPenalty")]
 #[repr(u32)]
 pub enum Penalty {
     /// No penalty.
     #[default]
+    #[enum_value(name = "Ok", nick = "ok")]
     Ok = 0,
     /// +2. Add two seconds penalty to the solve time.
+    #[enum_value(name = "+2", nick = "plus-two")]
     Plus2 = 1,
     /// Did not finish (DNF)
+    #[enum_value(name = "DNF", nick = "dnf")]
     Dnf = 2,
 }
