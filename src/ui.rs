a64907a2e08458c9537d1c135f26c12dfe6fd30a
ManEnfu <manenfu@protonmail.com>
2024-01-21 10:05:40 +0700

    feat: add penalty selector widget

--- src/ui.rs
+++ src/ui.rs
@@ -1,5 +1,7 @@
+pub use penalty_selector::PenaltySelector;
 pub use session_item_row::SessionItemRow;
 pub use timer_face::TimerFace;
 
+mod penalty_selector;
 mod session_item_row;
 mod timer_face;
