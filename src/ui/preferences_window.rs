e69ae6c7f75dae82e033f4d697dafe32882cbf3b
ManEnfu <manenfu@protonmail.com>
2024-02-09 18:03:00 +0700

    feat: add color scheme settings into gschema

--- src/ui/preferences_window.rs
+++ src/ui/preferences_window.rs
@@ -1,9 +1,13 @@
 use adw::prelude::*;
 use adw::subclass::prelude::*;
-use gtk::{gdk, glib};
+use gtk::{gdk, gio, glib};
+
+use crate::config;
 
 #[doc(hidden)]
 mod imp {
+    use once_cell::sync::OnceCell;
+
     use super::*;
 
     #[derive(Debug, Default, gtk::CompositeTemplate)]
@@ -13,6 +17,8 @@ mod imp {
         pub(super) use_system_color_scheme_switch: TemplateChild<adw::SwitchRow>,
         #[template_child]
         pub(super) dark_mode_switch: TemplateChild<adw::SwitchRow>,
+
+        pub(super) settings: OnceCell<gio::Settings>,
     }
 
     #[glib::object_subclass]
@@ -43,6 +49,7 @@ mod imp {
 
             let obj = self.obj();
 
+            obj.setup_settings();
             obj.setup_appearance_group();
         }
     }
@@ -66,19 +73,48 @@ impl PreferencesWindow {
             .build()
     }
 
+    fn setup_settings(&self) {
+        let imp = self.imp();
+
+        let settings = gio::Settings::new(config::APP_ID);
+        imp.settings
+            .set(settings)
+            .expect("`settings` should not be set before `setup_settings` is called");
+    }
+
     fn setup_appearance_group(&self) {
         let imp = self.imp();
         let manager = adw::StyleManager::default();
+        let settings = self.settings();
+
+        settings
+            .bind("dark-mode", &*imp.dark_mode_switch, "active")
+            .build();
 
         if manager.system_supports_color_schemes() {
             imp.use_system_color_scheme_switch.set_sensitive(true);
             imp.use_system_color_scheme_switch
-                .bind_property("active", &imp.dark_mode_switch.get(), "sensitive")
+                .bind_property("active", &*imp.dark_mode_switch, "sensitive")
                 .sync_create()
                 .invert_boolean()
+                .build();
+
+            settings
+                .bind(
+                    "use-system-color-scheme",
+                    &*imp.use_system_color_scheme_switch,
+                    "active",
+                )
                 .build();
         } else {
             imp.use_system_color_scheme_switch.set_sensitive(false);
         }
+    }
+
+    fn settings(&self) -> &gio::Settings {
+        self.imp()
+            .settings
+            .get()
+            .expect("`settings` should be set by `setup_settings` first")
     }
 }
