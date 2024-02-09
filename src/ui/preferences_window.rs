12c052721cad280102447d21af00a4745b1d9c74
ManEnfu <manenfu@protonmail.com>
2024-02-09 14:47:18 +0700

    feat: add color scheme entries in preferences window

--- src/ui/preferences_window.rs
+++ src/ui/preferences_window.rs
@@ -8,7 +8,12 @@ mod imp {
 
     #[derive(Debug, Default, gtk::CompositeTemplate)]
     #[template(resource = "/io/github/manenfu/PrismaTimer/ui/preferences_window.ui")]
-    pub struct PreferencesWindow {}
+    pub struct PreferencesWindow {
+        #[template_child]
+        pub(super) use_system_color_scheme_switch: TemplateChild<adw::SwitchRow>,
+        #[template_child]
+        pub(super) dark_mode_switch: TemplateChild<adw::SwitchRow>,
+    }
 
     #[glib::object_subclass]
     impl ObjectSubclass for PreferencesWindow {
@@ -32,7 +37,16 @@ mod imp {
         }
     }
 
-    impl ObjectImpl for PreferencesWindow {}
+    impl ObjectImpl for PreferencesWindow {
+        fn constructed(&self) {
+            self.parent_constructed();
+
+            let obj = self.obj();
+
+            obj.setup_appearance_group();
+        }
+    }
+
     impl WidgetImpl for PreferencesWindow {}
     impl WindowImpl for PreferencesWindow {}
     impl AdwWindowImpl for PreferencesWindow {}
@@ -50,5 +64,21 @@ impl PreferencesWindow {
         glib::Object::builder()
             .property("transient-for", window)
             .build()
+    }
+
+    fn setup_appearance_group(&self) {
+        let imp = self.imp();
+        let manager = adw::StyleManager::default();
+
+        if manager.system_supports_color_schemes() {
+            imp.use_system_color_scheme_switch.set_sensitive(true);
+            imp.use_system_color_scheme_switch
+                .bind_property("active", &imp.dark_mode_switch.get(), "sensitive")
+                .sync_create()
+                .invert_boolean()
+                .build();
+        } else {
+            imp.use_system_color_scheme_switch.set_sensitive(false);
+        }
     }
 }
